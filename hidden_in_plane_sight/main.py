0f91dec2dba8136c3779836431c4ce8470703219
ravenstudios <ravenstudiosrob@yahoo.com>
2023-06-12 16:13:18 -0500

    Finished

---
+++ hidden_in_plane_sight/main.py
@@ -0,0 +1,20 @@
+"""
+    So here is some lyrics that sums up really how I feel. Or is it. Maybe
+    somewhere in these lyrics may be another message.
+    Im so proud of how well you are learning this. SHUT UP!!!!! I know
+    what you are thinking and your wrong. Anyhow, I
+    have so much faith in you that if you figuer this out, I will
+    refund you 0.05% of that dollar you gave me. WINK
+"""
+
+
+lyrics = """I'm doing this tonight
+            You're probably gonna start a fight
+            I know this can't be right
+            Hey baby come on
+            I loved you endlessly
+            When you weren't there for me
+            So now it's time to leave and make it alone"""
+
+
+song = [65, 46, 39, 3, 46, 88, 46, 49]
