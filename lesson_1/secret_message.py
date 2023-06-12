5af00f296456847d568d5c9d8f27c97035f74487
ravenstudios <ravenstudiosrob@yahoo.com>
2023-06-12 15:52:06 -0500

    init commit

---
+++ lesson_1/secret_message.py
@@ -0,0 +1,50 @@
+"""
+Hello Beautiful. Below you will find a secret message.
+Your mission is to decode this message and text me
+the name of the album and artist.
+
+Since this is the first one we will take it easy.
+All you have to do is reverse the secret_message string below.
+
+https://www.w3schools.com/python/python_howto_reverse_string.asp
+
+But as you get better I will make these
+secret messages harder. ;) inseret joke here_____________
+
+
+I associate music with memories in my life. There are
+some songs I cant listen to because it hurts too much.
+Some songs, band, albums make me feel happy. There is
+an album that I've always loved and it always makes me happy.
+I got this album around the time we were togather. Im not
+sure if I every played it for you. This album is a mix of emotions
+and some lyrics of some songs hits home and REALLY makes me think of you.
+If you get a chance, please listen to this album and think of me. But only
+the good lyrics.
+"""
+
+
+
+secret_message = """moor ym dellif nevaeh ruoy thginot litnU
+legna na yb devol neeb reven ev'I
+erup os gnihtyna tlef reven ev'I
+legna na yb devol neeb reven ev'I tuB"""
+
+print(secret_message[5 :20])
+
+
+
+
+
+
+
+
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+
+print(arr)
+result = []
+for a in arr:
+    result.append(a * 2)
+print(result)
---
+++ lesson_2/no_fucking_cheating.py
@@ -0,0 +1,31 @@
+"""
+Hello Beautiful, I want to tell you something important. But I dont want
+anybody else to know, so I made another secret message just for you.
+To understand the message you need to loop through the list
+below and call chr() for every item in this list
+and append each item converted into a char into a variable
+you can print out to find the message im trying to tell you.
+
+These are ascii numbers that repersent letters and symbols.
+This is how a computer uses letters
+
+!!!!!!!!!!!!!!!!!!!!!!!! USE THIS LINK !!!!!!!!!!!!!!!!!!!
+https://www.asciitable.com/
+
+!!!!!!!!!!!!!!!!!!!!!!!! USE THIS LINK !!!!!!!!!!!!!!!!!!!
+https://www.w3schools.com/python/ref_func_chr.asp
+
+I.E.
+
+Lets say we have a list with numbes [72, 105]
+If you look at the ascii table you can see that 72 = "H" and 105 = "i"
+
+HINT: you will need a variable to hold each letter I.E. result = "",
+then result += each item you call chr() on.
+
+Listen, no fucking cheating this time. I put my heart into
+thing of what song I want you to listen to and know the message
+I'm trying to tell you.
+If you cheat this time you'll get a good spanking.
+"""
+song = [104, 116, 116, 112, 115, 58, 47, 47, 119, 119, 119, 46, 121, 111, 117, 116, 117, 98, 101, 46, 99, 111, 109, 47, 119, 97, 116, 99, 104, 63, 118, 61, 115, 118, 121, 76, 77, 82, 55, 121, 106, 104, 85, 53]
